1336e555fc14bf0bd5b956f2800858d3a09a929d
sarahjelle <sarahj.98@hotmail.com>
2020-07-07 11:28:55 +0200

    Lenker på alle avsnitt og andre av typen block

--- schemas/avsnitt_tilleggsstønad.js
+++ schemas/avsnitt_tilleggsstønad.js
@@ -7,7 +7,48 @@ export default {
             title: 'Innhold',
             name: 'avsnitt_innhold',
             type: 'array',
-            of: [{ type: 'block' }],
+            of: [
+                {
+                    type: 'block',
+                    marks: {
+                        annotations: [
+                            {
+                                name: 'link',
+                                type: 'object',
+                                title: 'External link',
+                                fields: [
+                                    {
+                                        name: 'href',
+                                        type: 'url',
+                                        title: 'URL'
+                                    },
+                                    {
+                                        title: 'Open in new tab',
+                                        name: 'blank',
+                                        type: 'boolean'
+                                    }
+                                ]
+                            },
+                            {
+                                name: 'internalLink',
+                                type: 'object',
+                                title: 'Internal link',
+                                fields: [
+                                    {
+                                        name: 'reference',
+                                        type: 'reference',
+                                        title: 'Reference',
+                                        to: [
+                                            { type: 'artikkel' },
+                                            // other types you may want to link to
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                }
+            ]
         },
         {
             name: 'knapp',
