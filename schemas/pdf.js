8f3e3ed06f88e212b914f0fa8ac74ac3466dda4b
ViktorGSolberg <viktorgsolberg@gmail.com>
2020-07-22 10:12:23 +0200

    Støtte for pdf

---
+++ schemas/pdf.js
@@ -0,0 +1,12 @@
+export default {
+    name: 'pdf',
+    title: 'PDF',
+    type: 'file',
+    fields: [
+      {
+        name: 'pdfnavn',
+        type: 'string',
+        title: 'Navn på PDF'
+      },
+    ]
+  }
